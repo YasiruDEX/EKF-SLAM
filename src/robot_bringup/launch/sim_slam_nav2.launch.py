0e23f048e79aec8189e0b7a3f20d875167fd423e
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2026-03-04 09:36:12 +0530

    update

--- src/robot_bringup/launch/sim_slam_nav2.launch.py
+++ src/robot_bringup/launch/sim_slam_nav2.launch.py
@@ -2,12 +2,21 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation time if true'
+    )
+
     bringup_share = get_package_share_directory('robot_bringup')
     slam_share = get_package_share_directory('robot_slam')
     nav_share = get_package_share_directory('robot_nav')
@@ -21,17 +30,24 @@ def generate_launch_description():
     slam_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(slam_share, 'launch', 'slam.launch.py')
-        )
+        ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+        }.items()
     )
 
     nav_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(nav_share, 'launch', 'navigation.launch.py')
-        )
+        ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+        }.items()
     )
 
     return LaunchDescription([
-        sim_launch,
+        use_sim_time_arg,
+        # sim_launch,
         TimerAction(period=2.0, actions=[slam_launch]),
         TimerAction(period=4.0, actions=[nav_launch]),
     ])
